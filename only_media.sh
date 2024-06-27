ae4f97822b00588129f5910e46174e9a6f2d4490
aigarskadikis <aigars.kadikis@gmail.com>
2024-06-27 09:10:52 +0300

    first

---
+++ only_media.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# remove old dir. start fresh
+rm -rf /tmp/7.0.zip
+rm -rf /tmp/zabbix-release-7.0
+
+# generate a SID by using:
+# "Administration" => "General" => "API tokens"
+SID=$(cat ~/.z70auth)
+
+# API endpoint
+JSONRPC=$(cat ~/.z70url)
+
+# download latest 7.0 branch
+curl -kL https://github.com/zabbix/zabbix/archive/refs/heads/release/7.0.zip -o /tmp/7.0.zip
+
+# unzip
+cd /tmp
+unzip 7.0.zip
+
+# go back to previous directory where PHP program is located
+cd -
+
+# start template import
+find /tmp/zabbix-release-7.0/templates/media -type f -name '*.yaml' | \
+while IFS= read -r MEDIA
+do {
+php media_type.php $SID $JSONRPC $MEDIA | jq .result | grep "true"
+# if 'true' not received the print the template name
+[[ $? -ne 0 ]] && echo $MEDIA
+} done
+
