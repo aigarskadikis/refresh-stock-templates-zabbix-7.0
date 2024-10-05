4114a3c67ae4d57be46152ded50ecaec7439f312
catonrug <aigars.kadikis@gmail.com>
2024-10-05 16:19:33 +0300

    next

--- refresh_stock.sh
+++ refresh_stock.sh
@@ -25,17 +25,16 @@ cd -
 find /tmp/zabbix-release-7.0/templates -type f -name '*.yaml' | \
 while IFS= read -r TEMPLATE
 do {
-php delete_missing.php $SID $JSONRPC $TEMPLATE | jq .result | grep "true" | tr '\n' '\0'
-echo " " | tr '\n' '\0'
+php delete_missing.php $SID $JSONRPC $TEMPLATE | jq .result | grep "true" > /dev/null && echo "OK $TEMPLATE" 
 # if 'true' not received the print the template name
-[[ $? -ne 0 ]] && echo -e "\n$TEMPLATE\n"
+[[ $? -ne 0 ]] && echo "failed $TEMPLATE"
 } done
 
 find /tmp/zabbix-release-7.0/templates/media -type f -name '*.yaml' | \
 while IFS= read -r MEDIA
 do {
-php media_type.php $SID $JSONRPC $MEDIA | jq .result | grep "true"
+php media_type.php $SID $JSONRPC $MEDIA | jq .result | grep "true" > /dev/null && echo "OK $MEDIA"
 # if 'true' not received the print the template name
-[[ $? -ne 0 ]] && echo $MEDIA
+[[ $? -ne 0 ]] && echo "failed $MEDIA"
 } done
 
