ef893dd398c9e250f524579610e75761463c80cc
Barak Griffis <b@griff.sh>
2026-01-24 09:18:03 -0600

    feat: update shell script to auto-detect release from zabbix api and pull correct version for that release

--- refresh_stock.sh
+++ refresh_stock.sh
@@ -9,34 +9,41 @@ set -o xtrace
 # read current directory to automatically understand where is php file
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+RELEASE=$(/usr/bin/curl -s -X POST -H "Content-Type: application/json" \
+	-d '{"jsonrpc":"2.0","method":"apiinfo.version","params":{},"id":1}' \
+	"$(cat ~/.zabbix-url)/api_jsonrpc.php" \
+	| jq -r .result | sed -E 's/\.[0-9]+$//' )
+
+echo "RELEASE=${RELEASE}"
+
 # remove old directory
-rm -rf /tmp/zabbix-release-7.0
+rm -rf /tmp/zabbix-release-${RELEASE}
 
 # make fresh directory
-mkdir -p /tmp/zabbix-release-7.0
+mkdir -p /tmp/zabbix-release-${RELEASE}
 
 # generate static session token by using menu "Users" => "API tokens"
-SID=$(cat ~/.zabbix-7.0-auth)
+SID=$(cat ~/.zabbix-auth)
 
 # frontend endpoint
-JSONRPC=$(cat ~/.zabbix-7.0-url)/api_jsonrpc.php
+JSONRPC=$(cat ~/.zabbix-url)/api_jsonrpc.php
 
-# download latest 7.0 branch from official repository of vendor
+# download latest ${RELEASE} branch from official repository of vendor
 curl --insecure \
 --location \
---output "/tmp/zabbix-release-7.0/7.0.zip" \
-"https://git.zabbix.com/rest/api/latest/projects/ZBX/repos/zabbix/archive?at=refs%2Fheads%2Frelease%2F7.0&format=zip"
+--output "/tmp/zabbix-release-${RELEASE}/${RELEASE}.zip" \
+"https://git.zabbix.com/rest/api/latest/projects/ZBX/repos/zabbix/archive?at=refs%2Fheads%2Frelease%2F${RELEASE}&format=zip"
 
 # unzip
-cd "/tmp/zabbix-release-7.0"
-unzip "7.0.zip"
-rm -rf "/tmp/zabbix-release-7.0/7.0.zip"
+cd "/tmp/zabbix-release-${RELEASE}"
+unzip "${RELEASE}.zip"
+rm -rf "/tmp/zabbix-release-${RELEASE}/${RELEASE}.zip"
 
 # do not print detailed commands
 set +o xtrace
 
 # start template import
-find /tmp/zabbix-release-7.0/templates -type f -name '*.yaml' | \
+find /tmp/zabbix-release-${RELEASE}/templates -type f -name '*.yaml' | \
 while IFS= read -r TEMPLATE
 do {
 php "$SCRIPT_DIR/delete_missing.php" "$SID" "$JSONRPC" "$TEMPLATE" | \
@@ -50,5 +57,5 @@ grep "true" > /dev/null && echo "OK $TEMPLATE"
 set -o xtrace
 
 # remove working directory
-rm -rf "/tmp/zabbix-release-7.0"
+rm -rf "/tmp/zabbix-release-${RELEASE}"
 
